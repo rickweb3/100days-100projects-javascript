4638a41eb742ca42464ae83843e866c56dd0b4dd
Henrique Prado <henrique.mxnero@gmail.com>
2021-07-23 21:49:19 -0300

    Day 1 - Aplicando a cor no count de acordo com o seu valor: vermelho - negativo, amarelo - positivo, branco - zero (neutro)

--- Day 1 - Counter App/script.js
+++ Day 1 - Counter App/script.js
@@ -7,18 +7,29 @@ const buttons = document.querySelector(".buttons");
 buttons.addEventListener("click", (e) => {
     if (e.target.classList.contains("add")) {
         count.innerHTML++;
+        setColor();
     }
 
     if (e.target.classList.contains("subtract")) {
         count.innerHTML--;
+        setColor();
     }
 
     if (e.target.classList.contains("reset")) {
         count.innerHTML = 0;
+        setColor();
     }
 });
 
-
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.color = "yellow";
+    } else if (count.innerHTML < 0) {
+        count.style.color = "orangered";
+    } else {
+        count.style.color = "white";
+    }
+}
 
 // add.addEventListener("click", () => {
 //     count.innerHTML++;
