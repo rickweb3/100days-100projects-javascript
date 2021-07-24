13b07a5e684a7bdcca845c6b0734f6ae3194e007
Henrique Prado <henrique.mxnero@gmail.com>
2021-07-23 21:44:34 -0300

    Aplicação do javascript para realizar as funções de adicionar, subtrair e resetar o contador

---
+++ Day 1 - Counter App/script.js
@@ -0,0 +1,32 @@
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const subtract = document.querySelector(".subtract");
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+    if (e.target.classList.contains("add")) {
+        count.innerHTML++;
+    }
+
+    if (e.target.classList.contains("subtract")) {
+        count.innerHTML--;
+    }
+
+    if (e.target.classList.contains("reset")) {
+        count.innerHTML = 0;
+    }
+});
+
+
+
+// add.addEventListener("click", () => {
+//     count.innerHTML++;
+// });
+// subtract.addEventListener("click", () => {
+//     count.innerHTML--;
+// });
+
+// resetCount.addEventListener("click", () => {
+//     count.innerHTML = 0;
+// })
