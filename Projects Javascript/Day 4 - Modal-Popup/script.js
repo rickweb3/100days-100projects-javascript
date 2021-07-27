2d0c1e5ebdb82d44f70b05565b88755f3dd5dd31
Henrique Prado <henrique.mxnero@gmail.com>
2021-07-27 11:52:49 -0300

    Funcionalidade Javascript para habilitar e desabilitar o modal

---
+++ Projects Javascript/Day 4 - Modal-Popup/script.js
@@ -0,0 +1,21 @@
+// VARIABLES
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".btn");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+
+// OPEN MODAL
+function openModal(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+
+// CLOSE MODAL
+function closeModal(e) {
+    modal.style.display = "none";
+}
